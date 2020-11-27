42743ee88e2d15200c6d5c136bab53d4139d1ee6
daviddongguo <david.dong.guo@gmail.com>
2020-11-27 14:25:06 -0500

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-interface Color{
-  red: number;
-  blue: number;
-  yellow: number;
-}
+import 'express';
+import 'express-validator';
+import 'jsonwebtoken';
 
-const myColor: Color = {
-  red: 255,
-  blue: 255,
-  yellow: 255
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
 
-};
 
-console.log(myColor);
-
-export default myColor;
--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,20 +1,37 @@
 import {NextFunction, Request, Response} from 'express';
-import {UserDoc} from '../models/user';
-import {verifyToken} from '../services/token';
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+	id: string;
+	email: string;
+}
 
 declare global {
-  namespace Express{
-    interface Request {
-      currentUser?: UserDoc | null;
-    }
-  }
+	namespace Express {
+		interface Request {
+			currentUser?: UserPayload | null;
+		}
+	}
 }
 
 export const currentUser = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+	req: Request,
+	res: Response,
+	next: NextFunction
 ) => {
-  req.currentUser = verifyToken(req.session?.jwt);
-  return next();
+  const token = req.session?.jwt;
+  if(!token){
+    console.log('token is not provided!');
+    req.currentUser = null;
+    return next();
+  }
+
+	let payload: UserPayload | null = null;
+	try {
+		payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!) as UserPayload;
+	} catch (error) {
+    console.log('token is INVALID!');
+  }
+	req.currentUser = payload;
+	return next();
 };
