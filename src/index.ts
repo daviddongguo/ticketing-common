2fc2b740161638b9eded1a422512ffb22debf160
daviddongguo <david.dong.guo@gmail.com>
2020-11-27 11:12:17 -0500

    additional config

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+interface Color{
+  red: number;
+  blue: number;
+  yellow: number;
+}
+
+const myColor: Color = {
+  red: 255,
+  blue: 255,
+  yellow: 255
+
+};
+
+console.log(myColor);
+
+export default myColor;
